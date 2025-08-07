461ad863f77548bb9c9bda2e7703ebd011ac7c5f
D1stort19 <wangjiaqi02@qq.com>
2025-08-08 03:11:14 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 using namespace std;
 
+struct node {
+    int left, right;
+} nodes[1000];
+
+bool empty_node[1000];
+int n;
+
 int main() {
-    string s;
-    cin >> s;
-    cout << s;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int parent, child;
+        cin >> parent >> child;
+        if (!empty_node[parent]) {
+            empty_node[parent] = !empty_node[parent];
+            nodes[parent].left = child;
+        } else {
+            nodes[parent].right = child;
+        }
+    }
     return 0;
 }
