20c2202ca5ad71d8c0ede6b2a7876c6537cdacba
D1stort19 <wangjiaqi02@qq.com>
2025-08-08 03:33:04 +0800

    update

--- main.cpp
+++ main.cpp
@@ -2,23 +2,56 @@
 using namespace std;
 
 struct node {
-    int left, right;
+    int parent;
+    int left_child, right_child;
 } nodes[1000];
 
-bool empty_node[1000];
 int n;
+int x, y;
+int x_depth, y_depth;
+int level[1000];
+int depth, width;
+
+void dfs(int node, int lv) {
+    if (!node) {
+        return;
+    }
+    level[lv]++;
+    if (lv > depth) {
+        depth = lv;
+    }
+    if (node == x) {
+        x_depth = lv;
+    }
+    if (node == y) {
+        y_depth = lv;
+    }
+    dfs(nodes[node].left_child, lv + 1);
+    dfs(nodes[node].right_child, lv + 1);
+}
+
 
 int main() {
     cin >> n;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n - 1; i++) {
         int parent, child;
         cin >> parent >> child;
-        if (!empty_node[parent]) {
-            empty_node[parent] = !empty_node[parent];
-            nodes[parent].left = child;
+        if (!nodes[parent].left_child) {
+            nodes[parent].left_child = child;
         } else {
-            nodes[parent].right = child;
+            nodes[parent].right_child = child;
         }
+        nodes[child].parent = parent;
     }
+    cin >> x >> y;
+    dfs(1, 1);
+    int lv = 1;
+    while (level[lv] != 0) {
+        if (level[lv] > width) {
+            width = level[lv];
+        }
+        lv++;
+    }
+    cout << depth << '\n' << width << '\n' << (x_depth - 1) * 2 + (y_depth - 1);
     return 0;
 }
