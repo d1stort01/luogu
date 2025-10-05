aae5717489c823516978d9f167a1e05791268bb7
d1stort <wangjiaqi02@qq.com>
2025-10-06 02:06:14 +0800

    fedora

--- main.cpp
+++ main.cpp
@@ -1,56 +1,47 @@
 #include <iostream>
-#include <vector>
+#include <unordered_set>
 
 using namespace std;
 
-class DisjointSets {
-public:
-    explicit DisjointSets(int size) : parents(size), sizes(size, 1) {
-        for (int i = 0; i < size; i++) {
-            parents[i] = i;
-        }
+int ds[1001];
+int sizes[1001];
+
+int find(int x) {
+    return ds[x] == x ? x : ds[x] = find(ds[x]);
+}
+
+void unite(int x, int y) {
+    int x_root = find(x);
+    int y_root = find(y);
+
+    if (x_root == y_root) return;
+
+    if (sizes[x_root] > sizes[y_root]) {
+        swap(x_root, y_root);
     }
 
-    int find(int x) {
-        return parents[x] == x ? x : (parents[x] = find(parents[x]));
-    }
-
-    bool unite(int x, int y) {
-        int x_root = find(x);
-        int y_root = find(y);
-        if (x_root == y_root) {
-            return false;
-        }
-        if (sizes[x_root] < sizes[y_root]) {
-            swap(x_root, y_root);
-        }
-        sizes[x_root] += sizes[y_root];
-        parents[y_root] = x_root;
-        return true;
-    }
-
-    bool connected(int x, int y) {
-        return find(x) == find(y);
-    }
-
-private:
-    vector<int> parents;
-    vector<int> sizes;
-};
+    ds[x_root] = y_root;
+}
 
 int main() {
-    DisjointSets ds(5005);
-    int n, m, p;
-    cin >> n >> m >> p;
-    while (m--) {
-        int mi, mj;
-        cin >> mi >> mj;
-        ds.unite(mi, mj);
-    }
-    while (p--) {
-        int pi, pj;
-        cin >> pi >> pj;
-        cout << (ds.connected(pi, pj) ? "Yes" : "No") << '\n';
-    }
+    int n, m;
+    while (cin >> n) {
+        if (n == 0) break;
+        cin >> m;
+        for (int i = 1; i <= n; i++) {
+            ds[i] = i;
+            sizes[i] = 1;
+        }
+        while (m--) {
+            int i, j;
+            cin >> i >> j;
+            unite(i, j);
+        }
+        unordered_set<int> s;
+        for (int i = 1; i <= n; i++) {
+            s.insert(find(i));    
+        }
+        cout << s.size() - 1 << '\n';
+    }    
     return 0;
 }
