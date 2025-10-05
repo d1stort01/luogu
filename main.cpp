6d572b75852b2239235836cd01965068b4a05145
d1stort <wangjiaqi02@qq.com>
2025-10-05 23:24:18 +0800

    win11
    Signed-off-by: d1stort <wangjiaqi02@qq.com>

--- main.cpp
+++ main.cpp
@@ -4,16 +4,14 @@
 using namespace std;
 
 class DisjointSets {
-  private:
-    vector<int> parents;
-    vector<int> sizes;
-
-  public:
-    DisjointSets(int size) : parents(size), sizes(size, 1) {
-        for (int i = 0; i < size; i++) { parents[i] = i; }
+public:
+    explicit DisjointSets(int size) : parents(size), sizes(size, 1) {
+        for (int i = 0; i < size; i++) {
+            parents[i] = i;
+        }
     }
 
-    int find(int x) { 
+    int find(int x) {
         return parents[x] == x ? x : (parents[x] = find(parents[x]));
     }
 
@@ -24,7 +22,7 @@ class DisjointSets {
             return false;
         }
         if (sizes[x_root] < sizes[y_root]) {
-            swap(x_root, y_root); 
+            swap(x_root, y_root);
         }
         sizes[x_root] += sizes[y_root];
         parents[y_root] = x_root;
@@ -32,20 +30,24 @@ class DisjointSets {
     }
 
     bool connected(int x, int y) {
-        return find(x) == find(y); 
+        return find(x) == find(y);
     }
+
+private:
+    vector<int> parents;
+    vector<int> sizes;
 };
 
 int main() {
-    DisjointSets ds(5001);
+    DisjointSets ds(5005);
     int n, m, p;
     cin >> n >> m >> p;
-    while(m--) {
+    while (m--) {
         int mi, mj;
         cin >> mi >> mj;
         ds.unite(mi, mj);
     }
-    while(p--) {
+    while (p--) {
         int pi, pj;
         cin >> pi >> pj;
         cout << (ds.connected(pi, pj) ? "Yes" : "No") << '\n';
